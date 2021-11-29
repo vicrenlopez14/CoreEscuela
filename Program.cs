33c1869e4933b89df3dc17b9627d390fa4378aac
Victor Lopez <vicrenlopez14115@gmail.com>
2021-11-29 15:36:08 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+        public string ceo;
+
+        public void Timbrar()
+        {
+            // TODO
+            Console.Beep(2000, 3000);
+        }
+    }
+    class Program
+    
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Platzi Academy";
+            miEscuela.direccion = "Cr 9 calle 72";
+            miEscuela.añoFundacion = 2012;
+
+            Console.Write("TIMBRE");
+            miEscuela.Timbrar();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
